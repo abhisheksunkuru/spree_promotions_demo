e897e9dec01a32e79be724a531ff4b3121aeb9fb
Abhishek <abhishek@railsfactory.org>
2016-06-01 15:30:02 +0530

    change config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,10 +77,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "https://spree-promotion-demo.herokuapp.com" }
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'gmail.com',
+    :domain               => 'heroku.com',
     :user_name            => 'qreport15@gmail.com',
     :password             => 'qreport123',
     :authentication       => 'plain',
