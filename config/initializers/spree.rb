67aea6fb44ca2cc400e28e1fb8a6bf7b7c2ed13b
Abhishek <abhishek@railsfactory.org>
2016-06-01 14:47:55 +0530

    spree paypal express

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -18,3 +18,5 @@ Rails.application.config.spree.promotions.rules <<
     Spree::Promotion::Rules::ProductWithOptionValueRule
 Rails.application.config.spree.calculators.promotion_actions_create_adjustments <<
     Spree::Calculator::ProductWithOptionValueCalculator
+
+Rails.application.config.spree.payment_methods << Spree::Gateway::PayPal
